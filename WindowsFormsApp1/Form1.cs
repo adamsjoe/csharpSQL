9e70a2ce7d8230fa873047a375d5929c58bfe289
Joseph Adams <joseph.adams@blueyonder.co.uk>
2020-11-12 22:07:04 +0000

    initial commit

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connetionString;
+            SqlConnection conn;
+            connetionString = @"Data Source=WINDOWS-R1VRDR1;Initial Catalog=theJoeDb;User ID=zoidberg;Password=joeisgreat";
+            conn = new SqlConnection(connetionString);
+            conn.Open();
+            //MessageBox.Show("Connection Open  !");
+
+            SqlCommand command;
+            SqlDataReader dataReader;
+            String sql, Output = "";
+
+            // sql we want to run
+            sql = "select * from testTable2";
+
+            // setup the command, using the sql above and the connection
+            command = new SqlCommand(sql, conn);
+
+            dataReader = command.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                Output = Output + dataReader.GetValue(0) + " - " + dataReader.GetValue(1) + "\n";
+            }
+
+            MessageBox.Show(Output);
+            conn.Close();
+        }
+    }
+}
